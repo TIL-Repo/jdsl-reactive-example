91585a924b02c141d09771f4c32cff3a98f8e370
joohyun.ha <mylifeforcoding@gmail.com>
2023-05-06 19:08:24 +0900

    init project

---
+++ src/main/kotlin/me/hajoo/jdslsample/JdslSampleApplication.kt
@@ -0,0 +1,11 @@
+package me.hajoo.jdslsample
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class JdslSampleApplication
+
+fun main(args: Array<String>) {
+    runApplication<JdslSampleApplication>(*args)
+}
---
+++ src/test/kotlin/me/hajoo/jdslsample/JdslSampleApplicationTests.kt
@@ -0,0 +1,13 @@
+package me.hajoo.jdslsample
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class JdslSampleApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
